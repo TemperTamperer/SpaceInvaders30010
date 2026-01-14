50635ea48c41817410c07596fee443ecca8d5c68
Benyamin ijaz <147488953+Kebbabb@users.noreply.github.com>
2026-01-14 10:34:49 +0100

    ingen ting

--- SpaceInvaders/Src/main.c
+++ SpaceInvaders/Src/main.c
@@ -1,11 +1,13 @@
-#include "joystick.h"
+
 #include "stm32f30x_conf.h" // STM32 config
 #include "30010_io.h" 		// Input/output library for this course
 #include <stdint.h>
+#include "joystick.h"
 
 int main(void)
 {
 	uart_init(9600);
+	 GPIO_init();
 
 	printf("Space invaders should go here");
 
