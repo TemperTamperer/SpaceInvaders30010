dbcf0f0f51fbb5ead5c171a4dd5d944eb90b95a4
LizzyBoi <gustavhectorbentzen@gmail.com>
2026-01-14 11:48:16 +0100

    Improvements to player_push_buffer. New funciton enemy_push_buffer added

--- SpaceInvaders/Src/enemy.c
+++ SpaceInvaders/Src/enemy.c
@@ -1,1 +1,21 @@
 #include "enemy.h"
+
+void enemy_push_buffer(uint8_t buffer[][SCREEN_COLS], enemy ene){
+
+	/*
+		  --< (XXX) >--
+		      ) X (
+		  --< (XVX) >--
+		*/
+		uint8_t alien_lasher[3][13] = {
+		    {'-','-','<',' ','(','X','X','X',')',' ','>','-','-'},
+		    {' ',' ',' ',' ',')',' ','X',' ','(',' ',' ',' ',' '},
+			{'-','-','<',' ','(','X','V','X',')',' ','>','-','-'},
+		};
+
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 13; j++){
+				buffer[ene.y - i][ene.x + j] = alien_lasher[2 - i][j];
+			}
+		}
+}
