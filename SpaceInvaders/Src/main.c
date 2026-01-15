511301934568f44c7215ad1751d24872125d63df
LizzyBoi <gustavhectorbentzen@gmail.com>
2026-01-15 13:04:22 +0100

    Working spawn function

--- SpaceInvaders/Src/main.c
+++ SpaceInvaders/Src/main.c
@@ -30,7 +30,9 @@ int main(void)
 
 	player p1 = {.x = 50, .y =SCREEN_ROWS-1, .sx =5, .sy = 3}; //defines the player at (50, 59) which is the bottom middle of the screen, with a size of 5x3
 	enemy enemy_pool[MAX_ENEMIES];
+	memset(enemy_pool, 0, sizeof(enemy_pool));
 	enemies_spawn(enemy_pool);
+
 
 	/*
 	enemy_pool[0] = (enemy){.x = 50, .y = 4, .alive = 1};
