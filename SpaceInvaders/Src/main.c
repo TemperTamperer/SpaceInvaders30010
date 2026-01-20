bd657674e550d5b5c2a122d20c5fc258b9308b47
Inferno0z0 <mostafaaag06@gmail.com>
2026-01-20 11:35:29 +0100

    Update main.c: Added Buzzer and sound

--- SpaceInvaders/Src/main.c
+++ SpaceInvaders/Src/main.c
@@ -7,15 +7,20 @@
 #include "timer.h"
 #include "joystick.h"
 #include "game_state.h"
-#include "asteroids.h"
-#include "game_state.h"
 
+#include "buzzer.h"
+#include "buzzer_songs.h"
+
+#include "asteroids.h"
 #include "enemy.h"
 #include "bullet.h"
+
 #include <stdint.h>
 #include <string.h>
 
 #define TICKS_PER_SECOND 20
+#define TICK_MS          (1000 / TICKS_PER_SECOND)
+
 #define POWERUP_SECONDS  5
 #define POWERUP_TICKS    (POWERUP_SECONDS * TICKS_PER_SECOND)
 #define ENEMY_BULLET_POOL_SIZE 16
@@ -38,103 +43,103 @@ int main(void)
     draw_border();
     printf("\x1B[?25l");
 
+    /* ===== BUZZER INIT + BG MUSIC ===== */
+    buzzer_init();
+    buzzer_set_bg(BG_MAIN_THEME, 1);   // loop baggrundsmusik
+    buzzer_bg_start();
+
     uint8_t current_buffer[SCREEN_ROWS][SCREEN_COLS];
     uint8_t shadow_buffer[SCREEN_ROWS][SCREEN_COLS];
     memset(current_buffer, ' ', SCREEN_ROWS * SCREEN_COLS);
     memset(shadow_buffer,  ' ', SCREEN_ROWS * SCREEN_COLS);
 
     player p1 = {.x = 50, .y = SCREEN_ROWS - 1, .sx = 5, .sy = 3};
-           p1.hp = 3;
-           p1.hit_count = 0;
+    p1.hp = 3;
+    p1.hit_count = 0;
+
     enemy enemy_pool[MAX_ENEMIES];
     memset(enemy_pool, 0, sizeof(enemy_pool));
-    asteroid ast = {.x = 2, .y = 20, .sx =9, .sy = 7, .alive = 1, .clean = 1};
 
+    asteroid ast = {.x = 2, .y = 20, .sx = 9, .sy = 7, .alive = 1, .clean = 1};
 
     /* Player bullets (din eksisterende pool) */
     Bullet* bullets = bullets_get_pool();
-    int bullet_count = bullets_get_count();
-    (void)bullet_count; // hvis du ikke bruger den direkte
-
+    (void)bullets_get_count();
     bullets_init(bullets, BULLET_POOL_SIZE);
 
     /* Enemy bullets (egen pool) */
     Bullet enemyBullets[ENEMY_BULLET_POOL_SIZE];
     bullets_init(enemyBullets, ENEMY_BULLET_POOL_SIZE);
+
     uint8_t prev_center_pressed = 0;
     uint8_t input = 0;
 
     game_state prev_state = -1;
 
-
     while (1)
     {
         if (!timer_flag)
-        	continue;
+            continue;
 
         timer_flag = 0;
+
+        /* ===== BUZZER TIMING =====
+           Jeres tick er 20 Hz => 50 ms pr tick */
+        buzzer_update((uint16_t)TICK_MS);
+
         input = read_joystick();
 
+        /* Hvis state ændres: tegn ny skærm */
         if (state != prev_state)
-                   {
-                       clrscr();
+        {
+            clrscr();
 
-                       if (state == STATE_BOSSKEY){
-                           boss_draw();
+            if (state == STATE_BOSSKEY) {
+                boss_draw();
+            }
+            else if (state == STATE_MENU) {
+                menu_draw();
+            }
+            else if (state == STATE_HELP) {
+                help_draw();
+            }
+            else if (state == STATE_PLAYING) {
+                draw_border();
+            }
+            prev_state = state;
+        }
 
-                       }
-                       else if (state == STATE_MENU){
-                           menu_draw();
-
-                       }
-                       else if (state == STATE_HELP){
-                           help_draw();
-
-                       }
-                       else if (state == STATE_PLAYING){
-                           draw_border();
-
-                       }
-                       prev_state = state;
-                   }
-
-        switch(state)
+        switch (state)
         {
         case STATE_MENU:
+            if (menu_update(input) == 1) {
+                clrscr();
+                draw_border();
+                state = STATE_PLAYING;
+            }
+            if (menu_update(input) == 2) {
+                clrscr();
+                draw_border();
+                state = STATE_HELP;
+            }
+            break;
 
-
-        	if (menu_update(input)==1)
-        	            {
-        	        	     clrscr();
-        	        	     draw_border();
-        	        	     state = STATE_PLAYING;
-        	        	}
-        	if (menu_update(input)==2)
-        	        	            {
-        	        	        	     clrscr();
-        	        	        	     draw_border();
-        	        	        	     state = STATE_HELP;
-        	        	        	 }
-        	            break;
         case STATE_HELP:
+            if (menu_update(input) == 1) {
+                clrscr();
+                draw_border();
+                state = STATE_PLAYING;
+            }
+            break;
 
-                	if (menu_update(input)==1)
-                	            {
-                	                clrscr();
-                	                draw_border();
-                	                state = STATE_PLAYING;
-                	            }
-                	break;
         case STATE_BOSSKEY:
-            if (menu_update(input) == 1)
-            {
+            if (menu_update(input) == 1) {
                 state = STATE_PLAYING;
             }
             break;
 
         case STATE_PLAYING:
-            if (menu_update(input) == 3)
-            {
+            if (menu_update(input) == 3) {
                 state = STATE_BOSSKEY;
                 break;
             }
@@ -144,8 +149,6 @@ int main(void)
 
             bonus_spawn_tick(enemy_pool);
 
-
-
             clear_buffer(current_buffer);
 
             /* Bevægelse: kun retninger (center påvirker ikke movement) */
@@ -153,67 +156,85 @@ int main(void)
             player_update_pos(move_input, &p1);
 
             /* Skyd (player) */
-            uint8_t center_just_pressed = joystick_just_pressed(input, JOY_CENTER, &prev_center_pressed);
+            uint8_t center_just_pressed =
+                joystick_just_pressed(input, JOY_CENTER, &prev_center_pressed);
+
             int startX = (p1.x + (p1.sx / 2));
             int startY = (p1.y - 1);
 
-            bullets_handle_shoot(bullets, BULLET_POOL_SIZE, center_just_pressed, startX, startY);
+            if (center_just_pressed) {
+                buzzer_play_sfx(SFX_SHOOT);
+            }
+
+            bullets_handle_shoot(bullets, BULLET_POOL_SIZE,
+                                 center_just_pressed, startX, startY);
+
             bullets_powerup_tick();
 
             bullets_update(bullets, BULLET_POOL_SIZE);
             bullets_update(enemyBullets, ENEMY_BULLET_POOL_SIZE);
 
             /* Enemy move/spawn */
-            if (enemy_move_counter > 15)
-            {
+            if (enemy_move_counter > 15) {
                 enemy_move_counter = 0;
                 enemies_update_pos(enemy_pool);
             }
 
-            if (enemy_spawn_counter > 80)
-            {
+            if (enemy_spawn_counter > 80) {
                 enemy_spawn_counter = 0;
                 enemies_spawn(enemy_pool);
             }
 
-            /* NYT: Fjender skyder (logikken ligger i enemy.c) */
+            /* Fjender skyder */
             enemies_shoot(enemy_pool, enemyBullets, ENEMY_BULLET_POOL_SIZE);
 
-            /* Opdater enemy bullets */
+            /* Enemy bullets + player hit SFX (detekter HP fald) */
+            uint8_t hp_before = p1.hp;
+
             bullets_update(enemyBullets, ENEMY_BULLET_POOL_SIZE);
-            player_hit_by_enemy_bullets(enemyBullets,  ENEMY_BULLET_POOL_SIZE, &p1);
-            /* Hit enemies med player bullets (din eksisterende) */
+            player_hit_by_enemy_bullets(enemyBullets, ENEMY_BULLET_POOL_SIZE, &p1);
+
+            if (p1.hp < hp_before) {
+                buzzer_play_sfx(SFX_PLAYER_HIT);
+            }
 
             asteroid_update_pos(&ast);
 
-            if (p1.hp == 0)
-            {
-                draw_game_over(score,highscore);
+            /* Game over */
+            if (p1.hp == 0) {
+                buzzer_play_sfx(SFX_GAMEOVER);
+                draw_game_over(score, highscore);
                 while (1) { } // stop spillet her (simpelt)
             }
-            int bonus_collected = 0;
-            int kills = bullets_hit_enemies(bullets, BULLET_POOL_SIZE, enemy_pool, &bonus_collected);
 
-            if (kills > 0)
-            {
+            /* Hit enemies med player bullets */
+            int bonus_collected = 0;
+            int kills = bullets_hit_enemies(bullets, BULLET_POOL_SIZE,
+                                           enemy_pool, &bonus_collected);
+
+            if (kills > 0) {
+                buzzer_play_sfx(SFX_ENEMY_DEATH);
+
                 score += (uint32_t)(kills * 10);
                 if (score > highscore)
                     highscore = score;
             }
 
-            if (bonus_collected)
+            if (bonus_collected) {
+                buzzer_play_sfx(SFX_BONUS);
                 bullets_powerup_activate(POWERUP_TICKS);
+            }
 
             /* TEST: bonus efter 20 kills */
             if (bullets_test_should_powerup(20))
                 bullets_powerup_activate(POWERUP_TICKS);
 
-            asteroid_enemies_collision(&ast, enemy_pool);    
+            asteroid_enemies_collision(&ast, enemy_pool);
+
             /* Draw */
             player_push_buffer(current_buffer, p1);
             enemies_push_buffer(current_buffer, enemy_pool);
 
-            /* Tegn først enemy bullets, så player bullets ovenpå (valgfrit) */
             bullets_push_buffer(current_buffer, enemyBullets, ENEMY_BULLET_POOL_SIZE);
             bullets_push_buffer(current_buffer, bullets, BULLET_POOL_SIZE);
             asteroid_push_buffer(current_buffer, ast);
