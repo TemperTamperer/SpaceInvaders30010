3ab410faec7e55507db1771d9f6670d8cab6de97
Inferno0z0 <mostafaaag06@gmail.com>
2026-01-22 13:45:16 +0100

    Update powerup.h

--- SpaceInvaders/Inc/powerup.h
+++ SpaceInvaders/Inc/powerup.h
@@ -4,12 +4,14 @@
 #include <stdint.h>
 #include "bullet.h"
 
+/* Powerup types */
 typedef enum {
     PU_NONE = 0,
     PU_TRIPLE_POS = 1,
     PU_SPREAD_5 = 2
 } PowerupType;
 
+/* Powerup state */
 typedef struct {
     PowerupType active;
     uint16_t ticks_left;
@@ -17,10 +19,12 @@ typedef struct {
     uint8_t trig_750;
 } PowerupState;
 
+/* Init and update */
 void powerup_init(PowerupState* s);
 void powerup_update_from_score(PowerupState* s, uint32_t score);
 void powerup_tick(PowerupState* s);
 
+/* Shooting behavior */
 void powerup_shoot(PowerupState* s,
                    Bullet bullets[], int count,
                    uint8_t shoot_just_pressed,
