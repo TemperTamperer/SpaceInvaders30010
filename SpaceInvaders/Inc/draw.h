32427d87e7bfad73be0d2496cf85583a594d8942
Inferno0z0 <mostafaaag06@gmail.com>
2026-01-22 13:39:45 +0100

    Update draw.h

--- SpaceInvaders/Inc/draw.h
+++ SpaceInvaders/Inc/draw.h
@@ -10,6 +10,7 @@
 #include "level.h"
 #include "asteroids.h"
 
+/* Draw full game frame */
 void draw_frame(uint8_t current_buffer[SCREEN_ROWS][SCREEN_COLS],
                 uint8_t shadow_buffer[SCREEN_ROWS][SCREEN_COLS],
                 const player *p,
@@ -20,17 +21,23 @@ void draw_frame(uint8_t current_buffer[SCREEN_ROWS][SCREEN_COLS],
                 const LevelState *level,
                 uint32_t score, uint32_t highscore);
 
+/* Screen control */
 void draw_game_init_screen(void);
 void draw_check_game_over(uint8_t hp, uint32_t score, uint32_t highscore);
+void draw_game_over(uint32_t score, uint32_t highscore);
 
-void draw_buffer(uint8_t buffer[][SCREEN_COLS], uint8_t shadow_buffer[][SCREEN_COLS]);
+/* Buffer handling */
+void draw_buffer(uint8_t buffer[][SCREEN_COLS],
+                 uint8_t shadow_buffer[][SCREEN_COLS]);
 void clear_buffer(uint8_t current_buffer[][SCREEN_COLS]);
 void draw_border(void);
 
+/* Level popup */
 void draw_level_box(uint8_t level);
 void draw_level_box_clear(void);
 
-void ui_draw_status(uint8_t hp, uint8_t hits, uint32_t score, uint32_t highscore);
-void draw_game_over(uint32_t score, uint32_t highscore);
+/* UI */
+void ui_draw_status(uint8_t hp, uint8_t hits,
+                    uint32_t score, uint32_t highscore);
 
 #endif
