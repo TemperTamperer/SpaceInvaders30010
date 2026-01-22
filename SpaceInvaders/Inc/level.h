38829106fe05a13e4235bbb560663bd9d66812d2
Inferno0z0 <mostafaaag06@gmail.com>
2026-01-22 13:43:49 +0100

    Update level.h

--- SpaceInvaders/Inc/level.h
+++ SpaceInvaders/Inc/level.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+/* Level state */
 typedef struct {
     uint8_t level;
     uint8_t prev_level;
@@ -10,12 +11,15 @@ typedef struct {
     uint8_t popup_ended_pulse;
 } LevelState;
 
+/* Init and update */
 void level_init(LevelState* s);
 void level_update_from_score(LevelState* s, uint32_t score);
 
+/* Level info */
 uint8_t level_get(const LevelState* s);
 uint16_t level_spawn_limit(const LevelState* s);
 
+/* Level popup */
 uint8_t level_popup_active(const LevelState* s);
 void level_popup_tick(LevelState* s);
 uint8_t level_popup_just_ended(LevelState* s);
