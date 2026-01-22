10f4fbc0561ec30da2e2d586d1a0fab0aa243f05
Inferno0z0 <mostafaaag06@gmail.com>
2026-01-22 13:26:55 +0100

    Update menu.c

--- SpaceInvaders/Src/menu.c
+++ SpaceInvaders/Src/menu.c
@@ -3,15 +3,19 @@
 #include "game_settings.h"
 #include <stdio.h>
 
+#define TL  "\xC9"   // top-left corner
+#define TR  "\xBB"   // top-right corner
+#define BL  "\xC8"   // bottom-left corner
+#define BR  "\xBC"   // bottom-right corner
+#define HL  "\xCD"   // horizontal line
+#define VL  "\xBA"   // vertical line
 
-#define TL  "\xC9"   // ╔
-#define TR  "\xBB"   // ╗
-#define BL  "\xC8"   // ╚
-#define BR  "\xBC"   // ╝
-#define HL  "\xCD"   // ═
-#define VL  "\xBA"   // ║"
+#define MENU_X 18
+#define MENU_Y 5
+#define MENU_W 44
+#define MENU_H 28
 
-
+// Draw helper
 static void draw_hline(int x, int y, int len)
 {
     gotoxy(x, y);
@@ -19,14 +23,15 @@ static void draw_hline(int x, int y, int len)
         printf(HL);
 }
 
+// Main menu screen
 void menu_draw(void)
 {
-    int x = 18;
-    int y = 5;
-    int w = 44;
-    int h = 28;
+    int x = MENU_X;
+    int y = MENU_Y;
+    int w = MENU_W;
+    int h = MENU_H;
 
-    /* Frame */
+    // Frame
     gotoxy(x, y);         printf(TL);
     draw_hline(x + 1, y, w - 2);
     printf(TR);
@@ -41,67 +46,39 @@ void menu_draw(void)
     draw_hline(x + 1, y + h - 1, w - 2);
     printf(BR);
 
-    /* Title */
+    // Title
     gotoxy(x + 12, y + 2);
     printf("SPACE  INVADERS");
 
-    /* Rocket ship */
-    gotoxy(x + 21, y + 8);  printf("^");
-    gotoxy(x + 20, y + 9);  printf("/#\\");
+    // Rocket ship
+    gotoxy(x + 21, y + 8);   printf("^");
+    gotoxy(x + 20, y + 9);   printf("/#\\");
     gotoxy(x + 19, y + 10);  printf("|=#=|");
     gotoxy(x + 20, y + 11);  printf("/ \\");
 
-    /* Enemies */
-    gotoxy(x + 14,  y + 4);  printf("\xDB\xDB  \xDB\xDB  \xDB\xDB  \xDB\xDB");
+    // Enemies
+    gotoxy(x + 14, y + 4);
+    printf("\xDB\xDB  \xDB\xDB  \xDB\xDB  \xDB\xDB");
 
-
-    /* Menu text */
+    // Menu text
     gotoxy(x + 10, y + 16);
     printf("CENTER  -  START GAME");
 
     gotoxy(x + 10, y + 18);
     printf("UP      -  HELP");
 
-    /* Footer */
+    // Footer
     gotoxy(x + 6, y + h - 2);
     printf("DTU 30010  Programming Project");
 }
 
+// Help screen
 void help_draw(void)
 {
-	draw_border();
+    draw_border();
 
     gotoxy(10, 10);
-    printf("To move use the joystick left and right");
+    printf("Move with the joystick left and right");
 
     gotoxy(10, 12);
-    printf("To shot press the joystick center(press stick down)");
-
-    gotoxy(20, 20);
-    printf("Press Center to start game");
-
-}
-void boss_draw(void)
-{
-
-	draw_border();
-
-    gotoxy(40, 20);
-    printf("SIKE");
-    gotoxy(60, 20);
-        printf("To continue press Center");
-
-
-}
-
-uint8_t menu_update(uint8_t input)
-{
-    // FIRE button starts game
-    if (input & JOY_CENTER)
-        return 1;
-    else if (input & JOY_UP)
-    	return 2;
-    else if (input & JOY_DOWN)
-    	return 3;
-    return 0;
-}
+    printf("Shoot with joystick center (press down)");
