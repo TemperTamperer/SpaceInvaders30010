f341b6d7f275447628020e732f630cdd9b0cdcb9
Inferno0z0 <mostafaaag06@gmail.com>
2026-01-22 13:40:56 +0100

    Update enemy.h

--- SpaceInvaders/Inc/enemy.h
+++ SpaceInvaders/Inc/enemy.h
@@ -1,14 +1,13 @@
 #ifndef ENEMY_H_
 #define ENEMY_H_
 
-
 #include <stdint.h>
 #include "game_settings.h"
 #include "player.h"
-#include "enemy.h"
 
 typedef struct Bullet Bullet;
 
+/* Enemy data */
 typedef struct
 {
     uint16_t x, y;
@@ -18,30 +17,36 @@ typedef struct
     uint8_t  has_bonus;
 } enemy;
 
+/* Enemy shooting state */
 typedef struct
 {
     uint16_t shoot_counter;
     int next_enemy;
 } EnemyShootState;
+
+/* Update helpers */
 void enemies_tick(enemy pool[],
                   uint16_t *move_counter,
                   uint16_t *spawn_counter,
-                  uint8_t spawn_limit);
+                  uint8_t spawn_limit,
+                  player *p);
 
 void enemies_init(enemy enemy_pool[]);
 void enemies_reset(enemy enemy_pool[], EnemyShootState* st);
 
-void enemies_push_buffer(uint8_t buffer[SCREEN_ROWS][SCREEN_COLS], enemy enemy_pool[]);
+/* Drawing */
+void enemies_push_buffer(uint8_t buffer[SCREEN_ROWS][SCREEN_COLS],
+                         enemy enemy_pool[]);
 
+/* Movement and spawning */
 void enemies_spawn(enemy enemy_pool[MAX_ENEMIES]);
-void enemies_update_pos(enemy enemy_pool[MAX_ENEMIES]);
-uint8_t enemies_player_collision(enemy enemy_pool[MAX_ENEMIES], player *p);
+void enemies_update_pos(enemy enemy_pool[MAX_ENEMIES], player *p);
 
+/* Shooting */
 void enemies_shoot(enemy enemy_pool[],
                    Bullet* enemyBullets,
                    int enemyBullets_n,
                    EnemyShootState* st,
                    uint8_t level);
 
-
-#endif /* ENEMY_H_ */
+#endif
