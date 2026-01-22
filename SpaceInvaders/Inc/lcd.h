fad0157799e1f8f51bae5c1743be604a9e5b72c3
Inferno0z0 <mostafaaag06@gmail.com>
2026-01-22 13:43:01 +0100

    Update lcd.h

--- SpaceInvaders/Inc/lcd.h
+++ SpaceInvaders/Inc/lcd.h
@@ -5,10 +5,15 @@
 #include "30010_io.h"
 #include <stdio.h>
 
+/* LCD drawing */
 void lcd_draw_score(int32_t score, uint8_t buffer[]);
-void lcd_update();
+void lcd_draw_heart(uint8_t health, uint8_t buffer[]);
+
+/* LCD helpers */
+void lcd_update(void);
 void lcd_scroll(uint8_t string[], int32_t *line);
-void lcd_draw_heart (uint8_t health, uint8_t buffer[]);
+
+/* LED control */
 void set_led(uint8_t input);
 
-#endif /* LCD_H_ */
+#endif
