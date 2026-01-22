0abe3700f989245df69227c87f781a9e10bdde45
Inferno0z0 <mostafaaag06@gmail.com>
2026-01-22 14:09:30 +0100

    Update enemy.h

--- SpaceInvaders/Inc/enemy.h
+++ SpaceInvaders/Inc/enemy.h
@@ -5,48 +5,39 @@
 #include "game_settings.h"
 #include "player.h"
 
+/* Forward declaration to avoid including bullet.h here */
 typedef struct Bullet Bullet;
 
-/* Enemy data */
+/* Enemy struct
+   Stores position, sprite size and state */
 typedef struct
 {
-    uint16_t x, y;
-    uint8_t  sx, sy;
-    uint8_t  hp;
-    uint8_t  alive;
+    uint16_t x, y;     // Position on screen
+    uint8_t  sx, sy;   // Sprite width and height
+    uint8_t  hp;       // Enemy health
+    uint8_t  alive;    // 1 = active, 0 = inactive
     uint8_t  has_bonus;
 } enemy;
 
-/* Enemy shooting state */
+/* Keeps track of enemy shooting order and timing */
 typedef struct
 {
-    uint16_t shoot_counter;
-    int next_enemy;
+    uint16_t shoot_counter; // Counts ticks between shots
+    int next_enemy;         // Index of next enemy that should shoot
 } EnemyShootState;
 
-/* Update helpers */
+/* Initialize all enemies (set inactive) */
+void enemies_init(enemy enemy_pool[]);
+
+/* Reset enemies and shooting state (used on level change / restart) */
+void enemies_reset(enemy enemy_pool[], EnemyShootState* st);
+
+/* Enemy tick function
+   Updates movement and spawning using counters */
 void enemies_tick(enemy pool[],
                   uint16_t *move_counter,
                   uint16_t *spawn_counter,
                   uint8_t spawn_limit,
                   player *p);
 
-void enemies_init(enemy enemy_pool[]);
-void enemies_reset(enemy enemy_pool[], EnemyShootState* st);
-
-/* Drawing */
-void enemies_push_buffer(uint8_t buffer[SCREEN_ROWS][SCREEN_COLS],
-                         enemy enemy_pool[]);
-
-/* Movement and spawning */
-void enemies_spawn(enemy enemy_pool[MAX_ENEMIES]);
-void enemies_update_pos(enemy enemy_pool[MAX_ENEMIES], player *p);
-
-/* Shooting */
-void enemies_shoot(enemy enemy_pool[],
-                   Bullet* enemyBullets,
-                   int enemyBullets_n,
-                   EnemyShootState* st,
-                   uint8_t level);
-
-#endif
+/* Spawns a new row of enemies at*
