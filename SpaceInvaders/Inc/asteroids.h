3a87f53198bf45594eada76d0008bbb812502555
Inferno0z0 <mostafaaag06@gmail.com>
2026-01-22 13:33:25 +0100

    Update asteroids.h

--- SpaceInvaders/Inc/asteroids.h
+++ SpaceInvaders/Inc/asteroids.h
@@ -2,23 +2,26 @@
 #define ASTEROIDS_H
 
 #include "game_settings.h"
-#include "stdint.h"
 #include "enemy.h"
 #include "bullet.h"
+#include <stdint.h>
 
-typedef struct{
-	uint16_t x, y; //position
-	uint16_t sx, sy; //dimensions
-	uint8_t alive; //on screen
-	uint8_t clean; //Is clean until enemy collision
+/* Asteroid data */
+typedef struct {
+    uint16_t x, y;    // position
+    uint16_t sx, sy;  // size
+    uint8_t  alive;   // active on screen
+    uint8_t  clean;   // used for collision state
 } asteroid;
 
+/* Asteroid functions */
 void asteroid_push_buffer(uint8_t buffer[][SCREEN_COLS], asteroid ast);
 void asteroid_update_pos(asteroid *ast);
 void asteroid_enemies_collision(asteroid *ast, enemy enemy_pool[MAX_ENEMIES]);
 void asteroid_gravity(Bullet bullets[], asteroid ast);
 int32_t get_approx_dist(int32_t x1, int32_t y1, int32_t x2, int32_t y2);
 
+/* Asteroid system state */
 typedef struct {
     asteroid ast;
     uint16_t move_counter;
@@ -27,6 +30,7 @@ typedef struct {
     uint32_t rng;
 } AsteroidSystem;
 
+/* Asteroid system control */
 void asteroids_init(AsteroidSystem *s);
 void asteroids_tick(AsteroidSystem *s, enemy enemy_pool[MAX_ENEMIES]);
 void asteroids_draw(const AsteroidSystem *s, uint8_t buffer[][SCREEN_COLS]);
