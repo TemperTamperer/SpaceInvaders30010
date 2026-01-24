2574a40b95680e81bf17b69a3828a2b151ea8553
Inferno0z0 <mostafaaag06@gmail.com>
2026-01-24 21:40:47 +0100

    Update enemy.h

--- SpaceInvaders/Inc/enemy.h
+++ SpaceInvaders/Inc/enemy.h
@@ -1,15 +1,16 @@
 #ifndef ENEMY_H_
 #define ENEMY_H_
 
-
 #include <stdint.h>
 #include "game_settings.h"
 
-struct player; // Forward declaration
+struct player;   // forward declaration
 typedef struct player player;
-struct Bullet;
+
+struct Bullet;   // forward declaration
 typedef struct Bullet Bullet;
 
+// enemy instance
 typedef struct enemy
 {
     uint16_t x, y;
@@ -19,11 +20,14 @@ typedef struct enemy
     uint8_t  has_bonus;
 } enemy;
 
+// shared enemy shooting state
 typedef struct
 {
     uint16_t shoot_counter;
     int next_enemy;
 } EnemyShootState;
+
+// update movement and spawning
 void enemies_tick(enemy pool[],
                   uint16_t *move_counter,
                   uint16_t *spawn_counter,
@@ -33,17 +37,9 @@ void enemies_tick(enemy pool[],
 void enemies_init(enemy enemy_pool[]);
 void enemies_reset(enemy enemy_pool[], EnemyShootState* st);
 
+// draw enemies
 void enemies_push_buffer(uint8_t buffer[SCREEN_ROWS][SCREEN_COLS], enemy enemy_pool[]);
 
 void enemies_spawn(enemy enemy_pool[MAX_ENEMIES]);
 void enemies_update_pos(enemy enemy_pool[MAX_ENEMIES], player *p);
-uint8_t enemies_player_collision(enemy enemy_pool[MAX_ENEMIES], player *p);
-
-void enemies_shoot(enemy enemy_pool[],
-                   Bullet* enemyBullets,
-                   int enemyBullets_n,
-                   EnemyShootState* st,
-                   uint8_t level);
-
-
-#endif /* ENEMY_H_ */
+uint8_t enemies_player_c_
